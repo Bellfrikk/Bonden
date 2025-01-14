85bd2173d3dc5bcbb5364bb1d59219e29e5b2458
Kjetil Aardal <kjetilaardal@gmail.com>
2025-01-14 10:30:23 +0100

    kart

---
+++ appen/felles/maskinar/fellesFunksjoner/hentFart.test.js
@@ -0,0 +1,34 @@
+"use strict";
+//import { sjekkAtFartIkkjeErOverMaks, sjekkOmFartSkalRundasTilNull, sjekkOmTomForDrivstoff, hentFart } from '../../../../felles/maskinar/fellesFunksjoner/hentFart.js';
+const hentFartTest = require('./felles/maskinar/fellesFunksjoner/hentFart');
+test('sjekk at samla fart ikkje er over maks for denne doningen', () => {
+    const fart = { aktiv: 100, friksjon: 0.1, maks: 100, landskap: 10, arbeid: 5, krasj: 0, aks: 0.1, tyngde: 0.1 };
+    expect(sjekkAtFartIkkjeErOverMaks(fart)).toBe(115);
+});
+test('sjekk om fart skal rundast til null nå den er under friksjonsveidiane', () => {
+    const skalBliNull = { aktiv: 0.1, friksjon: 0.1, krasj: 0.1, maks: 100, landskap: 10, arbeid: 5, aks: 0.1, tyngde: 0.1 };
+    const skalIkkjeBliNull = { aktiv: 1, friksjon: 0.1, krasj: 0.1, maks: 100, landskap: 10, arbeid: 5, aks: 0.1, tyngde: 0.1 };
+    expect(sjekkOmFartSkalRundasTilNull(skalBliNull)).toBe(0);
+    expect(sjekkOmFartSkalRundasTilNull(skalIkkjeBliNull)).toBe(1);
+});
+test('sjekk at fart bli redusert når drivstoffet er tomt', () => {
+    const fartAktiv = 100;
+    const harDrivstoff = 1;
+    const tomForDrivstoff = 0;
+    const negtivDrivstoff = -1;
+    expect(sjekkOmTomForDrivstoff(fartAktiv, harDrivstoff)).toBe(fartAktiv);
+    expect(sjekkOmTomForDrivstoff(fartAktiv, tomForDrivstoff)).toBe(straffeFartVedTomForDrivstoff);
+    expect(sjekkOmTomForDrivstoff(fartAktiv, negtivDrivstoff)).toBe(straffeFartVedTomForDrivstoff);
+});
+test('sjekk at rett fart blir henta', () => {
+    const fartOverMaks = { aktiv: 130, aks: 0.1, tyngde: 1, maks: 100, landskap: 10, arbeid: 5, friksjon: 0.1, krasj: 0 };
+    const fartUnderMaks = { aktiv: 30, aks: 0.1, tyngde: 1, maks: 100, landskap: 10, arbeid: 5, friksjon: 0.1, krasj: 0 };
+    const fartCaNull = { aktiv: 0.2, aks: 0.1, tyngde: 1, maks: 100, landskap: 0, arbeid: 0, friksjon: 0.1, krasj: 0.3 };
+    const lastFull = { leverer: ['drivstoff'], laster: { drivstoff: { niva: 10 } } };
+    const lastTom = { leverer: ['drivstoff'], laster: { drivstoff: { niva: 0 } } };
+    expect(hentFart(fartOverMaks, lastFull)).toBe(100);
+    expect(hentFart(fartUnderMaks, lastFull)).toBe(15);
+    expect(hentFart(fartCaNull, lastFull)).toBe(0);
+    expect(hentFart(fartOverMaks, lastTom)).toBe(straffeFartVedTomForDrivstoff);
+    expect(hentFart(fartCaNull, lastTom)).toBe(0);
+});
---
+++ appen/lagKart/lagKart.js
@@ -0,0 +1,28 @@
+"use strict";
+let nyttKart = tmpKart;
+const verktoyKnappar = ['vei', 'grus', 'asfalt', 'skog', 'vatn', 'eng', 'jorde'];
+function startLagKart() {
+    let antalX = tmpKart.antalRuter.x;
+    let antalY = tmpKart.antalRuter.y;
+    const verden = document.getElementById("verden");
+    const verktoy = document.createElement("div");
+    verktoy.id = "verktoyLinje";
+    verden.appendChild(verktoy);
+    const verktoyLinje = document.getElementById("verktoyLinje");
+    verktoyKnappar.forEach(type => {
+        const knapp = document.createElement("button");
+        knapp.id = type;
+        knapp.name = type;
+        verktoyLinje.appendChild(knapp);
+    });
+    for (let y = 0; y < antalY; y++) {
+        const rekke = document.createElement("div");
+        let id = "rekke" + y;
+        rekke.id = id;
+        verden.appendChild(rekke);
+        for (let x = 0; x < antalX; x++) {
+            const rute = document.createElement("div");
+            rekke.appendChild(rute);
+        }
+    }
+}
